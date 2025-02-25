add00a81256932758a554f19ff2e9462d7f1f778
Lior Dorenboust <Liordorenboust@gmail.com>
2025-02-25 15:08:25 +0200

    Initial project

--- app/api/getNonce/route.js
+++ app/api/getNonce/route.js
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-export async function GET(request) {
+export async function GET() {
   const nonce = crypto.randomBytes(16).toString('hex');
   return new Response(JSON.stringify({ nonce }), {
     headers: { "Content-Type": "application/json" },
