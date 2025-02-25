c905411d33ec03596ebd4718884831813ce2a1d8
Lior Dorenboust <Liordorenboust@gmail.com>
2025-02-25 15:19:23 +0200

    add CORS

--- app/api/getNonce/route.js
+++ app/api/getNonce/route.js
@@ -1,8 +1,22 @@
 import crypto from 'crypto';
 
-export async function GET() {
+export async function GET(request) {
   const nonce = crypto.randomBytes(16).toString('hex');
   return new Response(JSON.stringify({ nonce }), {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*", // Allow all origins; for production, restrict this
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+    },
+  });
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
   });
 }
