212830dfa28b40c5c6ed55fcc0a789bb3f07280f
Lior Dorenboust <Liordorenboust@gmail.com>
2025-03-06 15:28:29 +0200

    update contract

--- app/config/contractConfig.ts
+++ app/config/contractConfig.ts
@@ -13,13 +13,37 @@
  */
 export const MerchSmertContractAddresses: Record<SupportedNetwork, string> = {
   [SupportedNetwork.Ethereum]: "",
-  [SupportedNetwork.Sepolia]: "0x954887Fe59495a6990d627bd3F554a55B1bA7EF9",
+  [SupportedNetwork.Sepolia]: "0x3088E68216e94502ea7AA38e8139edF274acC4E4",
 };
 
 /**
  * The ABI for the 0xMerchSmert contract.
  */
 export const MERCH_ABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
   {
     "inputs": [
       {
@@ -132,6 +156,32 @@ export const MERCH_ABI = [
     "type": "function"
   },
   {
+    "inputs": [],
+    "name": "maxRoyalties",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
     "inputs": [
       {
         "internalType": "address",
@@ -226,6 +276,39 @@ export const MERCH_ABI = [
       }
     ],
     "name": "removeContract",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_newMaxRoyalties",
+        "type": "uint256"
+      }
+    ],
+    "name": "updateMaxRoyalties",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
